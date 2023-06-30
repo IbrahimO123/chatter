76b132ee27668176ec9c96066b489ee673f663ca
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-06-30 16:13:02 +0100

    confirm persist values

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,17 +1,57 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { chatPhotosSlice } from "./chatbox/slice";
 import { postSlice } from "./posts/slice";
 import { userSlice } from "./user/slice";
 import { otherSlice } from "./Others/slice";
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+
+
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_KEY || "scgsxvsndbcfgffshdvbcgscxfhvbxccfygacsr" ,
+      onError: (err) => {
+        console.log("err", err);
+      },
+    }),
+  ],
+};
+
+
+
+export const rootReducers = combineReducers({
+  chats: chatPhotosSlice.reducer,
+  posts: postSlice.reducer,
+  users: userSlice.reducer,
+  others: otherSlice.reducer,
+});
+
+const persistReducers = persistReducer(persistConfig, rootReducers, )
 
 export const store = configureStore({
-  reducer: {
-    chats: chatPhotosSlice.reducer,
-    posts: postSlice.reducer,
-    users: userSlice.reducer,
-    others: otherSlice.reducer,
-  },
+  reducer: persistReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
