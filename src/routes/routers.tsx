360aaaa9e51b04cb4f92e7dba9e6e4cbc7ecd1a0
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-07-04 14:41:03 +0100

    added the route for the write component

--- src/routes/routers.tsx
+++ src/routes/routers.tsx
@@ -3,12 +3,14 @@ import { Home } from "../view/Home";
 import { Login } from "../view/Login";
 import { SignUp } from "../view/SignUp";
 import { PrivateRoute } from "../components/PrivateRoute";
+import { WritePost } from "../view/Write";
 
 export const Routers = () => {
   return (
     <Routes>
-      <Route element={<PrivateRoute/>}>
+      <Route element={<PrivateRoute />}>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/write" element={<WritePost />}></Route>
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
