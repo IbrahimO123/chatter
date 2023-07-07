5222f5bfa49ed65963d454e3594a41541dccd6e8
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-07-07 05:59:13 +0100

    remove space

--- src/redux/user/model.tsx
+++ src/redux/user/model.tsx
@@ -1,20 +1,18 @@
 export type User = {
-   
-    firstname: string;
-    lastname: string;
-    email: string;
-    phoneNumber: string;
-    password: string;
-    confirmPassword: string;
-    isAuthorised: boolean;
-    isLoggedIn: boolean;
-    isRegistered: boolean;
-    dateCreated: string;
-    timeCreated: string;
-}
-
+  firstname: string;
+  lastname: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
+  isAuthorised: boolean;
+  isLoggedIn: boolean;
+  isRegistered: boolean;
+  dateCreated: string;
+  timeCreated: string;
+};
 
 export type AppUser = {
-    allUsers: User[];
-    aUser: User;
-}
+  allUsers: User[];
+  aUser: User;
+};
