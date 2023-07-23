3f53a241c2a87ad08a1de30b416214a27e573212
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-07-23 12:54:58 +0100

    add an editor component for markdown package

---
+++ src/components/Editors.tsx
@@ -0,0 +1,34 @@
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+import { RootState } from "../redux/store";
+import { useSelector, useDispatch } from "react-redux";
+import { updateArticle } from "../redux/articles/slice";
+
+import "react-markdown-editor-lite/lib/index.css";
+
+const mdParser = new MarkdownIt();
+
+type EditorChange = {
+  html: string;
+  text: string;
+};
+
+const EditorMarkdown = () => {
+  const dispatch = useDispatch();
+  const anArticle = useSelector((state: RootState) => state.articles.anArticle);
+  const {text} = anArticle;
+
+  const handleEditorChange = ({ html, text }: EditorChange) => {
+    dispatch(updateArticle({ ...anArticle, text: text }));
+  };
+
+  return (
+    <MdEditor
+      style={{ height: "600px" }}
+      renderHTML={(text) => mdParser.render(text)}
+      onChange={handleEditorChange}
+      value={text}
+    />
+  );
+};
+export default EditorMarkdown;
