45a6e516ba68de4f1dde270645d2bb9555d42cad
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-07-23 13:03:18 +0100

    trouble why state is not accesssible

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store , persistor} from "./redux/store";
 import { BrowserRouter as Router } from "react-router-dom";
-import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
-let persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
