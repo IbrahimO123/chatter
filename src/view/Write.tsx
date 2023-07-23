e5196ceec6e6743e592f23aed1d044bf92bc37f0
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-07-23 12:58:12 +0100

    add a write route to the app

--- src/view/Write.tsx
+++ src/view/Write.tsx
@@ -1,80 +1,198 @@
-import { Box, Typography, Grid, Paper, Button, IconButton } from "@mui/material";
-import { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import { ArrayColor } from "../Utilities/support";
+import {
+  Box,
+  Typography,
+  Grid,
+  Paper,
+  Select,
+  Button,
+  IconButton,
+  MenuItem,
+  InputLabel,
+  SelectChangeEvent,
+  TextField,
+  Chip,
+  OutlinedInput,
+} from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
 import EditNoteIcon from "@mui/icons-material/EditNote";
-import parse from "html-react-parser";
+import PublishIcon from "@mui/icons-material/Publish";
 
-export const WritePost = () => {
-  const [value, setValue] = useState("");
+import { Tags } from "../Utilities/support";
+
+import { RootState } from "../redux/store";
+import { updateArticle } from "../redux/articles/slice";
+import { useSelector, useDispatch } from "react-redux";
+
+import { updateOtherState } from "../redux/Others/slice";
+import EditorMarkdown from "../components/Editors";
+
+export const WriteArticle = () => {
+  const dispatch = useDispatch();
+  const { anArticle } = useSelector((state: RootState) => state.articles);
+  const aUser = useSelector((state: RootState) => state.users.aUser);
+  const others = useSelector((state: RootState) => state.others);
+  const { email, firstname, lastname } = aUser;
+  const { categories, title, text } = anArticle;
+  const handleCategory = (e: SelectChangeEvent<typeof Tags>) => {
+    const {
+      target: { value },
+    } = e;
+    if (categories.length < 2) {
+      dispatch(
+        updateArticle({
+          ...anArticle,
+          categories: typeof value === "string" ? value.split(",") : value,
+        })
+      );
+    } else {
+      dispatch(
+        updateOtherState({
+          ...others,
+          message: "You can only select two tags ",
+          open: true,
+          severity: "error",
+        })
+      );
+      dispatch(
+        updateArticle({
+          ...anArticle,
+          categories: [],
+        })
+      );
+    }
+    return;
+  };
+  const saveArticle = () => {
+    
+  }
+
+  const handleArticle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(
+      updateArticle({
+        ...anArticle,
+        [e.target.name]: e.target.value,
+        authorEmail: email,
+        authorName: `${firstname} ${lastname}`,
+      })
+    );
+  };
+  // useEffect(() => {
+  //   console.log("The article has been updated", anArticle);
+  //   console.log("Length of text", text.length);
+  // }, [text]);
   return (
     <Box>
-      <Typography component="h1" variant="h6" m={1}>
-        Write an Article
-      </Typography>
+      <Paper elevation={4}>
+        <Typography component="h1" variant="h6" m={1}>
+          Write an Article
+        </Typography>
 
-      <Grid container spacing={2}>
-        <Grid item md={2.4}>
-          <Paper sx={{ height: "70vh", marginLeft: "10px" }} elevation={8}>
-            <Typography component="h1" variant="h6" p={1}>
-              <IconButton>
-                <EditNoteIcon />
-              </IconButton>
-              Articles
-            </Typography>
-          </Paper>
-        </Grid>
-        <Grid item md={9.6}>
-          <Paper
-            elevation={8}
-            sx={{ height: "70vh", marginRight: "10px", padding: "10px" }}
-          >
-            <ReactQuill
-              className="editor"
-              theme="snow"
-              value={value}
-              onChange={setValue}
-              placeholder="Write out your magic words..."
-              modules={{
-                toolbar: {
-                  container: [
-                    [
-                      { header: "1" },
-                      { header: "2" },
-                      { header: [3, 4, 5, 6] },
-                      { font: [] },
-                    ],
-                    [{ size: [] }],
-                    ["bold", "italic", "underline", "strike", "blockquote"],
-                    [{ list: "ordered" }, { list: "bullet" }],
-                    ["link", "video"],
-                    ["link", "image"],
-                    [
-                      {
-                        color: ArrayColor,
-                      },
-                    ],
-                    ["clean"],
-                    ["code-block"],
-                  ],
-                },
+        <Grid container spacing={2}>
+          <Grid item md={2.4}>
+            <Paper
+              sx={{
+                height: "70vh",
+                marginLeft: "10px",
+                display: { md: "grid", xs: "none" },
               }}
-            />
-            <Button variant="contained" disableRipple disabled size="large">
-              Publish
-            </Button>
-          </Paper>
+              elevation={8}
+            >
+              <Typography component="h1" variant="h6" p={1}>
+                <IconButton>
+                  <EditNoteIcon />
+                </IconButton>
+                Articles
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item md={9.5}>
+            <Paper
+              elevation={8}
+              sx={{ marginBottom: "5px", padding: "0 5px 5px 5px" }}
+            >
+              <Typography variant="h4" component="h6">
+                Title
+              </Typography>
+              <TextField
+                placeholder="Write the title here...."
+                variant="standard"
+                fullWidth
+                name="title"
+                type="text"
+                value={title}
+                onChange={handleArticle}
+              ></TextField>
+            </Paper>
+            <Paper elevation={8} sx={{ marginRight: "10px", padding: "10px" }}>
+              <EditorMarkdown />
+            </Paper>
+
+            <Box m={2} component="div" sx={{ width: "25vw" }}>
+              <Box pb={1}>
+                <Button
+                  sx={{ margin: "5px" }}
+                  variant="contained"
+                  disabled={text.length < 200 ? true : false}
+                  size="small"
+                  endIcon={<PublishIcon />}
+                  color="success"
+                >
+                  Publish
+                </Button>
+                <Button
+                  sx={{
+                    margin: "5px",
+                    backgroundColor: "#311b92",
+                    "& :hover": { backgroundColor: "#311b92" },
+                  }}
+                  variant="contained"
+                  disabled={text.length < 200 ? true : false}
+                  size="small"
+                  endIcon={<SaveIcon />}
+                >
+                  Save
+                </Button>
+              </Box>
+              <InputLabel id="select-label">Category</InputLabel>
+              <Select
+                value={categories}
+                multiple
+                sx={{ width: { sm: "20vw" } }}
+                disabled={text.length < 200 ? true : false}
+                onChange={handleCategory}
+                input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                renderValue={(selected) => {
+                  if (selected.length === 0) {
+                    return <em>Placeholder</em>;
+                  }
+                  return (
+                    <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} />
+                      ))}
+                    </Box>
+                  );
+                }}
+                inputProps={{ "aria-label": "Without label" }}
+              >
+                <MenuItem disabled value="">
+                  <em>Select the Category</em>
+                </MenuItem>
+                {Tags.map((tag) => (
+                  <MenuItem value={tag} key={tag}>
+                    {tag}
+                  </MenuItem>
+                ))}
+              </Select>
+              <p style={{ lineHeight: "0" }}>
+                <small style={{ fontSize: "10px", color: "red" }}>
+                  select maximum of two tags
+                </small>
+              </p>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item md={12}>
-          <Box>
-            <Typography component="h1" variant="h6" m={1}>
-              Preview
-            </Typography>
-            <Typography>{parse(value)}</Typography>
-          </Box>
-        </Grid>
-      </Grid>
+      </Paper>
     </Box>
   );
 };
