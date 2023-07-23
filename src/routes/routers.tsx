bfb8e0b315fbfd4a4b6369ab0cc48063b2e4df98
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-07-23 12:58:57 +0100

    chnage writepost to write article

--- src/routes/routers.tsx
+++ src/routes/routers.tsx
@@ -3,14 +3,14 @@ import { Home } from "../view/Home";
 import { Login } from "../view/Login";
 import { SignUp } from "../view/SignUp";
 import { PrivateRoute } from "../components/PrivateRoute";
-import { WritePost } from "../view/Write";
+import { WriteArticle } from "../view/Write";
 
 export const Routers = () => {
   return (
     <Routes>
       <Route element={<PrivateRoute />}>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/write" element={<WritePost />}></Route>
+        <Route path="/write" element={<WriteArticle />}></Route>
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
