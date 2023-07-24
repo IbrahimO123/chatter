83c45aeba5cadcd2172e9bf48e23a365256f3a85
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-07-24 22:56:06 +0100

    added a function for saving article to database

--- src/Utilities/AddData.tsx
+++ src/Utilities/AddData.tsx
@@ -1,6 +1,7 @@
 import { collection, setDoc, doc } from "firebase/firestore";
 import { User } from "../redux/user/model";
 import { db } from "../config/firebase";
+import { Article } from "../redux/articles/model";
 
 export const addToDatabase = async (data: User, email: string) => {
   try {
@@ -10,5 +11,15 @@ export const addToDatabase = async (data: User, email: string) => {
   } catch (e: any) {
     console.error(e.message);
     return "failed...";
+  }
+};
+
+export const addArticletoDatabase = async (data: Article, email: string) => {
+  try {
+    const collectionRef = collection(db, "articles");
+    const res = await setDoc(doc(collectionRef, email), data);
+    return res;
+  } catch (e: any) {
+    console.error(e.message);
   }
 };
