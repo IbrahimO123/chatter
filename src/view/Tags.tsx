da623f3ff5880153437967a1d57acb936e9c0d0c
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-08-05 15:23:27 +0100

    tags page created

---
+++ src/view/Tags.tsx
@@ -0,0 +1,27 @@
+import {
+  Container,
+  Typography,
+  MenuList,
+  MenuItem,
+  Badge,
+} from "@mui/material";
+import { Tags } from "../Utilities/support";
+import TagIcon from "@mui/icons-material/Tag";
+
+export const TagsPage = () => {
+  return (
+    <Container className="tags">
+      <Typography mt={3}>Tags</Typography>
+      <MenuList>
+        {Tags &&
+          Tags.map((tags, index) => (
+            <MenuItem key={index}>
+              <Badge color="info" badgeContent={1}>
+                #{tags}
+              </Badge>
+            </MenuItem>
+          ))}
+      </MenuList>
+    </Container>
+  );
+};
