000929d43bc8c470f73c179ff216c2d1b401c31c
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-08-09 04:28:20 +0100

    modified the ui interface for writing article

--- src/view/Write.tsx
+++ src/view/Write.tsx
@@ -8,7 +8,7 @@ import {
   Button,
   IconButton,
   MenuItem,
-  InputLabel,
+  FormHelperText,
   SelectChangeEvent,
   TextField,
   Chip,
@@ -27,6 +27,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { updateOtherState } from "../redux/Others/slice";
 import EditorMarkdown from "../components/Editors";
 import { addArticletoDatabase } from "../Utilities/AddData";
+import { useState } from "react";
+import { UploadImage } from "../Utilities/UploadImage";
 
 export const WriteArticle = () => {
   const dispatch = useDispatch();
@@ -35,6 +37,15 @@ export const WriteArticle = () => {
   const others = useSelector((state: RootState) => state.others);
   const { email, firstname, lastname } = aUser;
   const { categories, title, text } = anArticle;
+  const [selectedImage, setSelectedImage] = useState<File>();
+
+  const handleSelectImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedImage(e.target!.files![0]);
+    if (selectedImage === undefined) {
+      console.log("No selected image");
+    }
+  };
+
   const handleCategory = (e: SelectChangeEvent<typeof Tags>) => {
     const {
       target: { value },
@@ -65,7 +76,9 @@ export const WriteArticle = () => {
     return;
   };
   const saveArticle = async () => {
-    await addArticletoDatabase(anArticle, title);
+    const photoUrl = await UploadImage(selectedImage, title);
+    await addArticletoDatabase({ ...anArticle, coverImage: photoUrl }, title);
+    console.log(anArticle);
     dispatch(
       updateOtherState({
         ...others,
@@ -92,125 +105,163 @@ export const WriteArticle = () => {
   //   console.log("What is the title: ", title);
   // }, [text]);
   return (
-    <Box>
-      <Paper elevation={4}>
-        <Typography textAlign="center" component="h1" variant="h6" m={1}>
-          Write an Article
-        </Typography>
+    <Box sx={{ margin: { xs: "10px", md: "5px" } }}>
+      <Typography textAlign="center" component="h1" variant="h6" m={1}>
+        Write an Article
+      </Typography>
 
-        <Grid container spacing={2}>
-          <Grid item md={2.4}>
-            <Paper
-              sx={{
-                height: "70vh",
-                marginLeft: "10px",
-                display: { md: "grid", xs: "none" },
-              }}
-              elevation={8}
-            >
-              <Typography component="h1" variant="h6" p={1}>
-                <IconButton>
-                  <EditNoteIcon />
-                </IconButton>
-                Articles
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item md={9.5}>
-            <Paper
-              elevation={8}
-              sx={{ marginBottom: "5px", padding: "0 5px 5px 5px" }}
-            >
+      <Grid container spacing={1}>
+        <Grid item md={2.4}>
+          <Paper
+            elevation={4}
+            sx={{
+              height: "80vh",
+              marginLeft: "10px",
+              display: { md: "grid", xs: "none" },
+            }}
+          >
+            <Typography component="h1" variant="h6" p={1}>
+              <IconButton>
+                <EditNoteIcon />
+              </IconButton>
+              Articles
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item md={9.5}>
+          <Box>
+            <Box sx={{ marginBottom: "10px" }}>
               <Typography sx={{ padding: "5px" }} variant="h5" component="h6">
                 Title
               </Typography>
               <TextField
                 placeholder="Write the title here...."
-                variant="standard"
-                fullWidth
+                sx={{ width: "50vw" }}
                 name="title"
                 type="text"
                 value={title}
+                variant="standard"
+                InputProps={{
+                  disableUnderline: true,
+                }}
                 onChange={handleArticle}
               ></TextField>
-            </Paper>
-            <Paper elevation={8} sx={{ marginRight: "10px", padding: "10px" }}>
-              <EditorMarkdown />
-            </Paper>
-
-            <Box m={2} component="div">
-              <Box pb={1}>
-                <Button
-                  sx={{ margin: "5px" }}
-                  variant="contained"
-                  disabled={
-                    text.length < 200
-                      ? true
-                      : false || title.length === 0 || categories.length === 0
-                  }
-                  size="small"
-                  endIcon={<PublishIcon />}
-                  color="success"
-                >
-                  Publish
-                </Button>
-                <Button
-                  color="warning"
-                  variant="contained"
-                  disabled={
-                    text.length < 200
-                      ? true
-                      : false || title.length === 0 || categories.length === 0
-                  }
-                  size="small"
-                  endIcon={<SaveIcon />}
-                  onClick={saveArticle}
-                >
-                  Save
-                </Button>
-              </Box>
-              <InputLabel id="select-label">Category</InputLabel>
-              <Select
-                value={categories}
-                multiple
-                sx={{ width: { xs: "50vw", md: "25vw" } }}
-                disabled={
-                  text.length < 200 ? true : false || title.length === 0
-                }
-                onChange={handleCategory}
-                input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                renderValue={(selected) => {
-                  if (selected.length === 0) {
-                    return <em>Placeholder</em>;
-                  }
-                  return (
-                    <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                      {selected.map((value) => (
-                        <Chip key={value} label={value} />
-                      ))}
-                    </Box>
-                  );
-                }}
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem disabled value="">
-                  <em>Select the Category</em>
-                </MenuItem>
-                {Tags.map((tag) => (
-                  <MenuItem value={tag} key={tag}>
-                    {tag}
-                  </MenuItem>
-                ))}
-              </Select>
-              <p style={{ lineHeight: "0" }}>
-                <small style={{ fontSize: "10px", color: "red" }}>
-                  select maximum of two tags
-                </small>
-              </p>
             </Box>
-          </Grid>
+
+            <Box sx={{ display: { xs: "block", md: "flex" } }}>
+              <Box>
+                <TextField
+                  sx={{ marginRight: "10px" }}
+                  type="file"
+                  helperText="Upload cover image"
+                  inputProps={{ accept: "image/*" }}
+                  onChange={handleSelectImage}
+                  variant="standard"
+                  InputProps={{
+                    disableUnderline: true,
+                  }}
+                ></TextField>
+              </Box>
+
+              <Box>
+                <Select
+                  value={categories}
+                  multiple
+                  sx={{
+                    width: { xs: "50vw", md: "25vw" },
+                    
+                  }}
+                  disabled={
+                    text.length < 200 ? true : false || title.length === 0
+                  }
+                  onChange={handleCategory}
+                  input={
+                    <OutlinedInput id="select-multiple-chip" label="Chip" />
+                  }
+                  renderValue={(selected) => {
+                    if (selected.length === 0) {
+                      return <em>Placeholder</em>;
+                    }
+                    return (
+                      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                        {selected.map((value) => (
+                          <Chip key={value} label={value} />
+                        ))}
+                      </Box>
+                    );
+                  }}
+                  inputProps={{
+                    "aria-label": "Without label",
+                  }}
+                >
+                  <MenuItem disabled value="">
+                    <em>Select the Category</em>
+                  </MenuItem>
+                  {Tags.map((tag) => (
+                    <MenuItem value={tag} key={tag}>
+                      {tag}
+                    </MenuItem>
+                  ))}
+                </Select>
+                <FormHelperText>
+                  Select the category for the article
+                </FormHelperText>
+                <Box sx={{ lineHeight: "0.5" }}>
+                  <small style={{ fontSize: "10px", color: "red" }}>
+                    select maximum of two tags
+                  </small>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+
+          <Paper sx={{ marginTop: "15px" }} elevation={4}>
+            <EditorMarkdown />
+            <FormHelperText sx={{ marginLeft: "5px" }}>
+              At least 200 characters
+            </FormHelperText>
+          </Paper>
+
+          <Box m={2} component="div">
+            <Box pb={1}>
+              <Button
+                sx={{ margin: "5px" }}
+                variant="contained"
+                disabled={
+                  text.length < 200
+                    ? true
+                    : false ||
+                      title.length === 0 ||
+                      categories.length === 0 ||
+                      selectedImage === undefined
+                }
+                size="small"
+                endIcon={<PublishIcon />}
+                color="success"
+              >
+                Publish
+              </Button>
+              <Button
+                color="warning"
+                variant="contained"
+                disabled={
+                  text.length < 200
+                    ? true
+                    : false ||
+                      title.length === 0 ||
+                      categories.length === 0 ||
+                      selectedImage === undefined
+                }
+                size="small"
+                endIcon={<SaveIcon />}
+                onClick={saveArticle}
+              >
+                Save
+              </Button>
+            </Box>
+          </Box>
         </Grid>
-      </Paper>
+      </Grid>
     </Box>
   );
 };
