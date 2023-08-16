285b7a873ee757eb971e0f8c500a85bfc6c2ca0c
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-08-16 05:36:51 +0100

    adjusted the editor for better ui

--- src/components/Editors.tsx
+++ src/components/Editors.tsx
@@ -4,19 +4,24 @@ import { RootState } from "../redux/store";
 import { useSelector, useDispatch } from "react-redux";
 import { updateArticle } from "../redux/articles/slice";
 
+
 import "react-markdown-editor-lite/lib/index.css";
 
-const mdParser = new MarkdownIt();
+
+export const mdParser = new MarkdownIt();
 
 type EditorChange = {
   html: string;
   text: string;
 };
 
+
 const EditorMarkdown = () => {
+ 
+
   const dispatch = useDispatch();
   const anArticle = useSelector((state: RootState) => state.articles.anArticle);
-  const { text } = anArticle;
+  const { text , } = anArticle;
 
   const handleEditorChange = ({ html, text }: EditorChange) => {
     dispatch(updateArticle({ ...anArticle, text, html }));
@@ -27,7 +32,18 @@ const EditorMarkdown = () => {
       style={{ height: "600px" }}
       renderHTML={(text) => mdParser.render(text)}
       onChange={handleEditorChange}
+      placeholder="Write the body article here support markdown!"
       value={text}
+      canView={{
+        menu: true,
+        md: true,
+        html: false,
+        fullScreen: true,
+        hideMenu: false,
+        both: false,
+      }}
+      view={{menu: true, md: true, html: false}}
+      
     />
   );
 };
