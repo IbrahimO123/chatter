9991422a450ed2082d6c0434be4889f1c52cefc7
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-08-20 15:20:49 +0100

    added reload draft functionality

--- src/components/Draft.tsx
+++ src/components/Draft.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   MenuList,
   MenuItem,
@@ -14,19 +14,69 @@ import { useSelector } from "react-redux";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditNoteIcon from "@mui/icons-material/EditNote";
 import { style } from "../Utilities/support";
+import { RetrieveAArticleOnce } from "../Utilities/RetrieveArticle";
+import { useDispatch } from "react-redux";
+import { updateArticle } from "../redux/articles/slice";
+import { updateOtherState } from "../redux/Others/slice";
 
 export const Draft = () => {
+  const dispatch = useDispatch();
   const { heading } = useSelector((state: RootState) => state.saveArticles);
+  const { anArticle } = useSelector((state: RootState) => state.articles);
+  const others = useSelector((state: RootState) => state.others);
+  const { title, } = anArticle;
   const [openModal, setOpenModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [modalAnswer, setModalAnswer] = useState(false);
   const handleModalOpen = () => {
     setOpenModal(true);
   };
+
+  useEffect(() => {
+    if (modalAnswer) {
+      handleDelete();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [modalAnswer]);
+
   const handleModalClose = () => {
     setOpenModal(false);
   };
+  const handleDeleteMsg = async (index: number) => {
+    setModalMessage("Are you sure you want to delete this draft?");
+    handleModalOpen();
+  };
+  const handleDelete = async () => {
+    console.log("delete draft");
+    handleModalClose();
+    setModalAnswer(false);
+  };
 
+  const handleReloadDraft = async (index: number) => {
+    const querySnapshot = await RetrieveAArticleOnce(heading[index]);
+    const result = heading.find((item: string) => item === title);
+    querySnapshot.forEach((doc) => {
+      console.log("what data are you", doc.data());
+      if (result !== undefined)
+        dispatch(
+          updateArticle({
+            ...anArticle,
+            ...doc.data(),
+          })
+        );
+      return dispatch(
+        updateOtherState({
+          ...others,
+          open: true,
+          message: "Article loaded successfully",
+          severity: "success",
+          loading: false,
+        })
+      );
+    });
+  };
   return (
-    <Box  >
+    <Box>
       <Typography component="h1" variant="h6" p={1}>
         <IconButton>
           <EditNoteIcon />
@@ -35,7 +85,11 @@ export const Draft = () => {
       </Typography>
       <MenuList>
         {heading.map((title: string, index: number) => (
-          <MenuItem sx={{ padding: "10px", fontSize:"12px" }} key={index}>
+          <MenuItem
+            onDoubleClick={() => handleReloadDraft(index)}
+            sx={{ padding: "10px", fontSize: "12px" }}
+            key={index}
+          >
             <Typewriter
               onInit={(typewriter) => {
                 typewriter
@@ -44,28 +98,31 @@ export const Draft = () => {
                   .start();
               }}
             />
-            <IconButton onClick={handleModalOpen}>
+            <IconButton onClick={() => handleDeleteMsg(index)}>
               <DeleteForeverIcon />
             </IconButton>
           </MenuItem>
         ))}
-      </MenuList>
-      <Modal
-        open={openModal}
-        onClose={handleModalClose}
-        aria-labelledby="delete article"
-        aria-describedby="delete drafted article"
-      >
-        <Box sx={{ ...style, width: 300 }}>
-          <Typography>Are you sure you want to delete the draft?</Typography>
-          <Box p={1} display="flex" justifyContent="space-around">
-            <Button color="error">Yes</Button>
-            <Button onClick={handleModalClose} sx={{ color: "#9e9e9e" }}>
-              No
-            </Button>
+        <Modal
+          open={openModal}
+          onClose={handleModalClose}
+          aria-labelledby="delete article"
+          aria-describedby="delete drafted article"
+          color="error"
+        >
+          <Box sx={{ ...style, width: { md: 300, xs: 200 } }}>
+            <Typography>{modalMessage}</Typography>
+            <Box p={1} display="flex" justifyContent="space-around">
+              <Button onClick={() => setModalAnswer(true)} color="error">
+                Yes
+              </Button>
+              <Button onClick={handleModalClose} sx={{ color: "#9e9e9e" }}>
+                No
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Modal>
+        </Modal>
+      </MenuList>
     </Box>
   );
 };
