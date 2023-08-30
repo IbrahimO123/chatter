79b4d996f39d601918ef47f240b65b190dff4e47
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-08-30 08:46:49 +0100

    reconfigure save draft

--- src/redux/articles/model.tsx
+++ src/redux/articles/model.tsx
@@ -24,9 +24,32 @@ export type AppArticle = {
   anArticle: Article;
 };
 
-export type SaveArticle = {
-  heading: Array<string>;
-  name: string;
-  email: string;
-  heading2: Array<string>;
+export type Draft = {
+  drafts: [
+    {
+      id: string;
+      data: [
+        {
+          text: string;
+          html: string;
+          title: string;
+          subtitle: string;
+          timeCreated: string;
+          dateCreated: string;
+          authorName: string;
+          authorEmail: string;
+          likes: number;
+          comments: {
+            text: Array<string>;
+            numberOfComments: number;
+          };
+          categories: Array<string>;
+          repost: number;
+          readOnly: boolean;
+          coverImage: string;
+          published: boolean;
+        }
+      ];
+    }
+  ];
 };
