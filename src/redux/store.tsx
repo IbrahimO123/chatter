2796e39cfc51d7dad87cd31f1de8357f0fa4e504
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-08-30 13:41:01 +0100

    use the secret key for encrypt

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -7,7 +7,8 @@ import { articleSlice } from "./articles/slice";
 import { saveDraftsSlice } from "./articles/slice";
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 import storageSession from "redux-persist/lib/storage/session";
-import { v4 as uuidv4 } from "uuid";
+
+
 
 import { encryptTransform } from "redux-persist-transform-encrypt";
 
@@ -22,13 +23,15 @@ import {
   persistStore,
 } from "redux-persist";
 
+
+
 const persistConfig = {
   key: "session",
   storage: storageSession,
   blacklist: ["users", "chats", "posts", "others"],
   transforms: [
     encryptTransform({
-      secretKey: uuidv4(),
+      secretKey: process.env.REACT_APP_SECRET_KEY!,
       onError: (err) => {
         console.log("err", err);
       },
@@ -36,27 +39,12 @@ const persistConfig = {
   ],
 };
 
-// const persistConfigStorage = {
-//   key: "save",
-//   storage: storage,
-//   blacklist: ["users", "chats", "posts", "others", "articles"],
-//   transforms: [
-//     encryptTransform({
-//       secretKey: process.env.REACT_APP_SECRET_KEY!,
-//       onError: (err) => {
-//         console.log("err", err);
-//       },
-//     }),
-//   ],
-// };
-
 export const sessionReducer = combineReducers({
   others: otherSlice.reducer,
   articles: articleSlice.reducer,
   chats: chatPhotosSlice.reducer,
   posts: postSlice.reducer,
   users: userSlice.reducer,
-  //saveArticles: persistReducer(persistConfigStorage, saveArticleSlice.reducer),
   saveDrafts: saveDraftsSlice.reducer,
 });
 
