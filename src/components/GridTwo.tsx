a8e7e3a1e7a824bb7ec339edaf0cef70c3e86603
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-08-30 08:43:08 +0100

    added auth state to monitor user activities

--- src/components/GridTwo.tsx
+++ src/components/GridTwo.tsx
@@ -11,6 +11,8 @@ import {
 } from "@mui/material";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 import womanImage from "../assets/images/woman.avif";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../config/firebase";
 
 type Photo = {
   albumId: number;
@@ -26,8 +28,8 @@ const postStyle = {
   flexDirection: "row",
 };
 
-
 export const GridTwo = () => {
+  const [user] = useAuthState(auth);
   const [photo, setPhoto] = useState<Photo>({} as Photo);
   const userPhoto = async () => {
     try {
@@ -35,7 +37,7 @@ export const GridTwo = () => {
       const res = await axios.get(api);
       const data = await res.data;
       setPhoto(data);
-    } catch (e:any) {
+    } catch (e: any) {
       console.error(e.message);
     }
   };
@@ -44,50 +46,52 @@ export const GridTwo = () => {
   }, [photo]);
   return (
     <Box>
-      <Paper
-        sx={{
-          display: { xs: "none", md: "grid" },
-          padding: "5px 5px",
-          margin: "10px",
-        }}
-        elevation={8}
-      >
-        <Grid container spacing={2}>
-          <Grid item md={1}>
-            <IconButton size="small">
-              <Avatar
-                title={photo.hasOwnProperty("title") ? `${photo.title}` : ""}
-                alt="user photo"
-                src={photo.hasOwnProperty("url") ? `${womanImage}` : ""}
-              />
+      {user?.uid ? (
+        <Paper
+          sx={{
+            display: { xs: "none", md: "grid" },
+            padding: "5px 5px",
+            margin: "10px",
+          }}
+          elevation={8}
+        >
+          <Grid container spacing={2}>
+            <Grid item md={1}>
+              <IconButton size="small">
+                <Avatar
+                  title={photo.hasOwnProperty("title") ? `${photo.title}` : ""}
+                  alt="user photo"
+                  src={photo.hasOwnProperty("url") ? `${womanImage}` : ""}
+                />
+              </IconButton>
+            </Grid>
+            <Grid item md={11}>
+              <TextField
+                fullWidth
+                size="small"
+                placeholder="Ask community for advice."
+              ></TextField>
+            </Grid>
+          </Grid>
+          <Box sx={postStyle}>
+            <Button endIcon={<PostAddIcon />} variant="contained">
+              Post
+            </Button>
+            <IconButton disableRipple={true} size="small">
+              Write article
             </IconButton>
-          </Grid>
-          <Grid item md={11}>
-            <TextField
-              fullWidth
-              size="small"
-              placeholder="Ask community for advice."
-            ></TextField>
-          </Grid>
-        </Grid>
-        <Box sx={postStyle}>
-          <Button endIcon={<PostAddIcon />} variant="contained">
-            Post
-          </Button>
-          <IconButton disableRipple={true} size="small">
-            Write article
-          </IconButton>
-          <IconButton disableRipple={true} size="small">
-            Photo
-          </IconButton>
-          <IconButton disableRipple={true} size="small">
-            Video
-          </IconButton>
-          <IconButton disableRipple={true} size="small">
-            Event
-          </IconButton>
-        </Box>
-      </Paper>
+            <IconButton disableRipple={true} size="small">
+              Photo
+            </IconButton>
+            <IconButton disableRipple={true} size="small">
+              Video
+            </IconButton>
+            <IconButton disableRipple={true} size="small">
+              Event
+            </IconButton>
+          </Box>
+        </Paper>
+      ) : null}
     </Box>
   );
 };
