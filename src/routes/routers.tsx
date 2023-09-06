77fe09be5bba6d4ae1222b49786bd83d33e29924
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-06 07:56:15 +0100

    added notfound and user component

--- src/routes/routers.tsx
+++ src/routes/routers.tsx
@@ -6,6 +6,9 @@ const Login = lazy(() => import("../view/Login"));
 const SignUp = lazy(() => import("../view/SignUp"));
 const WriteArticle = lazy(() => import("../view/Write"));
 const TagsPage = lazy(() => import("../view/Tags"));
+const User = lazy(() => import("../view/User"));
+const NotFound = lazy(() => import("../view/NotFound"));
+
 const Blog = lazy(() => import("../components/Blog"));
 const PrivateRoute = lazy(() => import("../components/PrivateRoute"));
 
@@ -15,6 +18,7 @@ export const Routers = () => {
       <Route path="/" element={<Home />}></Route>
       <Route element={<PrivateRoute />}>
         <Route path="/write" element={<WriteArticle />}></Route>
+        <Route path="/user" element={<User />}></Route>
       </Route>
       <Route path="/tags" element={<TagsPage />}></Route>
       <Route
@@ -30,6 +34,7 @@ export const Routers = () => {
       ></Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 };
