1f2060c68b994b7e47eaef1049f048b15c04ebe2
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-12 14:44:45 +0100

    ongoing work on use profile

--- src/view/User.tsx
+++ src/view/User.tsx
@@ -1,11 +1,11 @@
-import { Container, Typography, Grid , Paper, Avatar} from "@mui/material";
+import { Container, Grid , Paper, Avatar} from "@mui/material";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../config/firebase";
-import { useEffect, useState } from "react";
+import { useEffect,  } from "react";
 
 const User = () => {
   const [user] = useAuthState(auth);
-  const [profile, setProfile] = useState([user])
+ // const [profile, setProfile] = useState([user])
   useEffect(() => {
     console.log("User", user);
   // eslint-disable-next-line react-hooks/exhaustive-deps
