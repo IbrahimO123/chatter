6e5e57879c63fe5836748fcd97fc3f2b77391e60
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-13 14:15:56 +0100

    meta data for article

--- src/view/SingleArticle.tsx
+++ src/view/SingleArticle.tsx
@@ -36,7 +36,7 @@ const SingleArticle = () => {
     <>
       <MetaTags
         description="Article page, reader can access the content of an article, comment on it, bookmark for future refrences"
-        title={`Chatter | ${singleArticle?.title}`}
+        title={`Chatter | ${singleArticle[0]?.title}`}
         PageTitle="Article Page, details on article"
         typeOfPlatform="website"
         url={`/articles/single/${articleId}`}
