8b39e38c99ebbab39381f66f0d6856eff09550c1
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-15 17:22:23 +0100

    article card modification

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -3,11 +3,14 @@ import { MobileView } from "../Utilities/support";
 
 export const PostCard = (post: any) => {
   return (
-    <Container sx={{ textAlign: "center" }} >
-      <Typography m={2} component="h1" variant="h4">
+    <Container sx={{ textAlign: "center" }}>
+      <Typography m={2} component="p" variant="h3">
         {post.title}
       </Typography>
-      <Box>
+      <Typography component="h1" variant="h6">
+        {post.subtitle}
+      </Typography>
+      <Box mt={1} mb={1}>
         <img
           src={post.coverImage}
           style={{
@@ -18,10 +21,15 @@ export const PostCard = (post: any) => {
           alt={post.title}
         ></img>
       </Box>
-      <Typography component="h1" variant="h6">
-        {post.subtitle}
-      </Typography>
       <Typography textAlign="left">{post.text}</Typography>
+      <Box textAlign="left" mt={1}>
+        <Typography mr={2} color="primary" component="span">
+          &#x23;{post?.categories[0]}
+        </Typography>
+        <Typography color="primary" component="span">
+          &#x23;{post?.categories[1]}
+        </Typography>
+      </Box>
     </Container>
   );
 };
--- src/components/PostSkeleton.tsx
+++ src/components/PostSkeleton.tsx
@@ -2,22 +2,28 @@ import { Box, Skeleton } from "@mui/material";
 export const PostSkeleton = () => {
   return (
     <Box textAlign="center">
-      <Skeleton variant="text" sx={{ fontSize: "2.5rem", margin:"5px" }} />
+      <Skeleton variant="text" sx={{ fontSize: "2.5rem", margin: "5px" }} />
+      <Skeleton variant="text" sx={{ fontSize: "2.5rem", margin: "5px" }} />
       <Skeleton
         variant="rounded"
         width={1160}
         height={300}
         sx={{ margin: "5px" }}
       />
-      <Skeleton variant="text" sx={{ fontSize: "2.5rem", margin: "5px" }} />
+
       <Skeleton
-      animation="wave"
+        animation="wave"
         variant="rectangular"
         width={1160}
         height={800}
         sx={{ margin: "5px" }}
       />
-      <Skeleton variant="rounded" width={300} height={40} sx={{margin:"5px"}} />
+      <Skeleton
+        variant="rounded"
+        width={300}
+        height={40}
+        sx={{ margin: "5px" }}
+      />
     </Box>
   );
 };
