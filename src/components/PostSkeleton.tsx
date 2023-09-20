e8fbd938c1d649e0f2a7c8480eadf116f7a91c75
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-20 14:10:25 +0100

    centeralize the content

--- src/components/PostSkeleton.tsx
+++ src/components/PostSkeleton.tsx
@@ -1,13 +1,16 @@
 import { Box, Skeleton, Paper } from "@mui/material";
 export const PostSkeleton = () => {
   return (
-    <Paper elevation={0}>
+    <Paper
+      elevation={0}
+      sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}
+    >
       <Box textAlign="center">
         <Skeleton variant="text" sx={{ fontSize: "2.5rem", margin: "5px" }} />
         <Skeleton variant="text" sx={{ fontSize: "2.5rem", margin: "5px" }} />
         <Skeleton
           variant="rounded"
-          width={1160}
+          width={1120}
           height={300}
           sx={{ margin: "5px" }}
         />
@@ -15,9 +18,9 @@ export const PostSkeleton = () => {
         <Skeleton
           animation="wave"
           variant="rectangular"
-          width={1160}
+          width={1120}
           height={800}
-          sx={{ margin: "5px" }}
+          sx={{ margin: "5px", marginBottom: "5px" }}
         />
         <Skeleton
           variant="rounded"
