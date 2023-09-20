ddb318d146f586ea35c6bc6223eb97ad670a1d8d
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-20 17:34:43 +0100

    added try catch

--- src/Utilities/GetUserData.tsx
+++ src/Utilities/GetUserData.tsx
@@ -13,35 +13,35 @@ export const getData = async (email: string) => {
   }
 };
 
-export const getLoggedInUser = async ({
-  user,
-  dispatch,
-  aUser,
-}: any) => {
-  if (user) {
-    if (user.uid !== null) {
-      const res = await getData(user.email!);
-      if (res?.data() !== null) {
-        dispatch(
-          updateAUser({
-            ...aUser,
-            firstname: res?.data()?.firstname,
-            lastname: res?.data()?.lastname,
-            phoneNumber: res?.data()?.phoneNumber,
-            email: res?.data()?.email,
-            isLoggedIn: true,
-            isRegistered: res?.data()?.isRegistered,
-            isAuthorised: res?.data()?.isAuthorised,
-            dateCreated: res?.data()?.dateCreated,
-            timeCreated: res?.data()?.timeCreated,
-            profileImageUrl: res?.data()?.profileImageUrl,
-            facebookHandle: res?.data()?.facebookHandle,
-            twitterHandle: res?.data()?.twitterHandle,
-            linkedInHandle: res?.data()?.linkedInHandle,
-          })
-        );
+export const getLoggedInUser = async ({ user, dispatch, aUser }: any) => {
+  try {
+    if (user) {
+      if (user.uid !== null) {
+        const res = await getData(user.email!);
+        if (res?.data() !== null) {
+          dispatch(
+            updateAUser({
+              ...aUser,
+              firstname: res?.data()?.firstname,
+              lastname: res?.data()?.lastname,
+              phoneNumber: res?.data()?.phoneNumber,
+              email: res?.data()?.email,
+              isLoggedIn: true,
+              isRegistered: res?.data()?.isRegistered,
+              isAuthorised: res?.data()?.isAuthorised,
+              dateCreated: res?.data()?.dateCreated,
+              timeCreated: res?.data()?.timeCreated,
+              profileImageUrl: res?.data()?.profileImageUrl,
+              facebookHandle: res?.data()?.facebookHandle,
+              twitterHandle: res?.data()?.twitterHandle,
+              linkedInHandle: res?.data()?.linkedInHandle,
+            })
+          );
+        }
+        return "Done";
       }
-      return "Done"
     }
+  } catch (err: any) {
+    console.error("Erro while getting signin user", err.message);
   }
 };
