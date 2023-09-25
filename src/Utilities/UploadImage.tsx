9463c924fe3fb5a87f1808125bece57f470e20ad
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-26 00:28:46 +0100

    added unction for profile image upload

--- src/Utilities/UploadImage.tsx
+++ src/Utilities/UploadImage.tsx
@@ -1,7 +1,7 @@
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage } from "../config/firebase";
 
-export const UploadImage = async (file: any, filename: string) => {
+export const UploadCoverImage = async (file: any, filename: string) => {
   try {
     const imageRef = ref(storage, filename);
     const res = await uploadBytes(imageRef, file as Blob);
@@ -12,5 +12,21 @@ export const UploadImage = async (file: any, filename: string) => {
     return false;
   } catch (e: any) {
     console.log(e.message);
+  }
+};
+
+export const UploadProfileImage = async (file: any, filename: string) => {
+  try {
+    const photoRef = ref(storage, `profileImages/${filename}`);
+    const res = await uploadBytes(photoRef, file as Blob);
+    if (res) {
+      const url = await getDownloadURL(
+        ref(storage, `profileImages/${filename}`)
+      );
+      return url;
+    }
+    return false;
+  } catch (e: any) {
+    console.log("Error while uploading image", e.code);
   }
 };
