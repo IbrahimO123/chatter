833e1f4f98927d9f0da1d3057eba58bd56f699e2
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-26 00:11:37 +0100

    remove the encrypted package from the app

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,15 +2,11 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { chatPhotosSlice } from "./chatbox/slice";
 import { postSlice } from "./posts/slice";
 import { userSlice } from "./user/slice";
-import { otherSlice } from "./Others/slice";
+import { otherSlice } from "./others/slice";
 import { articleSlice } from "./articles/slice";
 import { saveDraftsSlice } from "./articles/slice";
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 import storageSession from "redux-persist/lib/storage/session";
-
-
-
-import { encryptTransform } from "redux-persist-transform-encrypt";
 
 import {
   persistReducer,
@@ -23,20 +19,10 @@ import {
   persistStore,
 } from "redux-persist";
 
-
-
 const persistConfig = {
   key: "session",
   storage: storageSession,
-  blacklist: ["users", "chats", "posts", "others"],
-  transforms: [
-    encryptTransform({
-      secretKey: process.env.REACT_APP_SECRET_KEY!,
-      onError: (err) => {
-        console.log("err", err);
-      },
-    }),
-  ],
+  blacklist: ["users", "chats", "posts", "others", "saveDrafts"],
 };
 
 export const sessionReducer = combineReducers({
