a71917503c19495f87d49b4f163102c4fc18bd12
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-09-26 13:35:17 +0100

    adjust the metat tag of the app for each page

--- src/components/MetaTag.tsx
+++ src/components/MetaTag.tsx
@@ -27,6 +27,10 @@ export const MetaTags = ({
       <meta property="og:description" content={description} />
       <meta property="og:image" content={Logo} />
       <meta property="og:url" content={url} />
+      <meta name="twitter:title" content={PageTitle} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={Logo} />
+      <meta name="twitter:card" content="summary_large_image" />
       <link rel="canonical" href={href} />
     </Helmet>
   );
