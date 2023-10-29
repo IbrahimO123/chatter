105384e220990a4a0b5c3a26079e3de749939e0e
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-10-29 19:55:01 +0100

    added some more function

--- src/custom/hooks/useArticle.tsx
+++ src/custom/hooks/useArticle.tsx
@@ -2,11 +2,14 @@ import { useGeneral } from "./useGeneral";
 import { updateArticle } from "../../redux/articles/slice";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
-import { updateOtherState } from "../../redux/Others/slice";
 import { Tags } from "../../Utilities/Miscellaneous";
 import { SelectChangeEvent } from "@mui/material";
 import { updateLike } from "../../redux/like/slice";
-// import { addLikeArticleToDatabase } from "../../Utilities/LikeArticle";
+import { getAllLikedArticle } from "../../Utilities/RetrieveLikedArticle";
+import { useState } from "react";
+import { LikeType } from "../../redux/like/model";
+import { addLikeArticleToDatabase } from "../../Utilities/AddLikeArticle";
+import { updateLikeAsync } from "../../redux/like/slice";
 
 export const useArticle = () => {
   const {
@@ -17,25 +20,29 @@ export const useArticle = () => {
     others,
     profileImageUrl,
     user,
+    updateOtherState,
   } = useGeneral();
 
   const { anArticle } = useSelector((state: RootState) => state.articles);
+  const [likedArticleList, setLikedArticleList] = useState<
+    LikeType["allLikes"]
+  >([]);
   const saveDrafts = useSelector((state: RootState) => state.saveDrafts);
   const { aLike } = useSelector((state: RootState) => state.like);
   const { value } = aLike;
   const { categories, title, text, subtitle, coverImage } = anArticle;
-  const handleArticle = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleWriteArticle = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(
       updateArticle({
         ...anArticle,
         [e.target.name]: e.target.value,
         authorEmail: email,
-        authorName: ` ${lastname} ${firstname}`,
+        authorName: `${lastname} ${firstname}`,
         profileImageUrl,
       })
     );
   };
-  const handleCategory = (e: SelectChangeEvent<typeof Tags>) => {
+  const handleSelectCategory = (e: SelectChangeEvent<typeof Tags>) => {
     const {
       target: { value },
     } = e;
@@ -65,36 +72,86 @@ export const useArticle = () => {
     return;
   };
 
-  const handleLikeArticle = async (articleId: string) => {
+  const handleGetUserLikedArticle = async (articleId: string) => {
     if (user?.uid) {
-      dispatch(
-        updateLike({
+      const response = await getAllLikedArticle(articleId);
+      if (response.error) {
+        console.log(response.error);
+        return;
+      } else {
+        response.likedArticle.map(async (liked: any) => {
+          if (liked.articleId === articleId && liked.whoId === user?.uid) {
+            return await dispatch(
+              updateLikeAsync({
+                ...aLike,
+                value: true,
+                articleId,
+                who: `${lastname} ${firstname}`,
+                whoId: user.uid,
+              })
+            );
+          } else {
+            dispatch(
+              updateLike({
+                ...aLike,
+                value: false,
+                articleId: "",
+                who: "",
+                whoId: " ",
+              })
+            );
+          }
+          return liked;
+        });
+      }
+    }
+  };
+
+  const handleUserLikeArticle = async (articleId: string, article: string) => {
+    if (user?.uid) {
+      const response = await dispatch(
+        updateLikeAsync({
           ...aLike,
           value: !value,
           articleId,
-          who: `${lastname} ${firstname}`,
+          article,
           whoId: user.uid,
+          who: `${lastname} ${firstname}`,
         })
       );
-
-      if (value) {
-        console.log("Not added to database");
-        console.log("Not a like", aLike);
-      } else {
-        console.log("Added to database");
-        console.log("A like", aLike);
-        //await addLikeArticleToDatabase(aLike, articleId);
-        return "liked";
-      }
+      const data = await JSON.parse(JSON.stringify(response.payload));
+      await addLikeArticleToDatabase(data, articleId);
+      return "liked";
     } else {
+      dispatch(
+        updateOtherState({
+          ...others,
+          open: true,
+          message: "Login first",
+          severity: "error",
+        })
+      );
       return false;
     }
   };
+  const handleFetchLikedArticles = async (articleId: string) => {
+    const response = await getAllLikedArticle(articleId);
+    const { likedArticle, error } = response;
+    if (error === null && likedArticle.length > 0) {
+      setLikedArticleList(likedArticle as LikeType["allLikes"]);
+      return;
+    } else {
+      return;
+    }
+  };
   return {
-    handleArticle,
-    handleCategory,
+    handleWriteArticle,
+    handleSelectCategory,
+    handleUserLikeArticle,
+    handleGetUserLikedArticle,
+    handleFetchLikedArticles,
+    updateLikeAsync,
     anArticle,
-    handleLikeArticle,
     categories,
     title,
     text,
@@ -102,5 +159,8 @@ export const useArticle = () => {
     coverImage,
     saveDrafts,
     value,
+    aLike,
+    likedArticleList,
+    setLikedArticleList,
   };
 };
