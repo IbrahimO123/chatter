2e27f27576d3ff8a618235a000d4f987b6546e57
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2024-01-15 04:32:52 +0100

    added droplist menu to the card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import {
   Avatar,
   Box,
+  Card,
+  CardMedia,
   Grid,
   IconButton,
   Paper,
@@ -11,6 +14,8 @@ import AddCommentOutlinedIcon from "@mui/icons-material/AddCommentOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { MenuComponent } from "./MenuComponent";
+import { PostMenu } from "../Utilities/Miscellaneous";
 
 type postCard = {
   likesCount?: number;
@@ -27,7 +32,15 @@ type postCard = {
   dateCreated?: string;
 };
 
-export const PostCard = ({ content, video }: postCard) => {
+export const PostCard = ({ content, video, picture }: postCard) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box>
       <Paper
@@ -37,46 +50,96 @@ export const PostCard = ({ content, video }: postCard) => {
         }}
         elevation={0}
       >
-        <Box sx={{display:"flex", justifyContent:"space-between", alignContent:"center"}}>
-          <Avatar
-            sizes="small"
-            src=""
-            sx={{ padding: "2px", margin: "10px 10px 0px 10px" }}
-          ></Avatar>
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
-        </Box>
-        <Grid container textAlign="center">
-          <Grid item md={12} xs={12}>
-            <Box component="div" p={2} textAlign="left">
-              <Typography>{content}</Typography>
+        <Card>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignContent: "center",
+            }}
+          >
+            <Avatar
+              sizes="small"
+              src=""
+              sx={{ padding: "2px", margin: "10px 10px 0px 10px" }}
+            ></Avatar>
+            <Box>
+              <IconButton
+                aria-label="more"
+                id="long-button"
+                aria-controls={open ? "long-menu" : undefined}
+                aria-expanded={open ? "true" : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <MoreVertIcon />
+              </IconButton>
+              <MenuComponent
+                open={open}
+                handleClose={handleClose}
+                data={PostMenu}
+                anchorEl={anchorEl}
+              />
             </Box>
+          </Box>
+          <Grid container textAlign="center">
+            <Grid item md={12} xs={12}>
+              <Box component="div" p={2} pb={0} textAlign="left">
+                <Typography>{content}</Typography>
+              </Box>
+              <Box p={2}>
+                {video ? (
+                  <center>
+                    <CardMedia
+                      component="video"
+                      sx={{ width: { xs: "300px", md: "500px" } }}
+                      controls
+                    >
+                      <source src={video} type="video/mp4" />
+                      <source src={video} type="video/webm" />
+                      <p>Your browser doesn't support HTML5 video.</p>
+                    </CardMedia>
+                  </center>
+                ) : null}
+              </Box>
+              <Box p={2}>
+                {picture ? (
+                  <center>
+                    <CardMedia
+                      component="img"
+                      sx={{ height: 350}}
+                      image={picture}
+                    ></CardMedia>
+                  </center>
+                ) : null}
+              </Box>
+            </Grid>
+            <Grid item md={12} xs={12}>
+              <Box
+                component="div"
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignContent: "center",
+                }}
+                pb={2}
+              >
+                <IconButton title="Like">
+                  <ThumbUpOutlinedIcon />
+                </IconButton>
+                <IconButton title="Add Comment">
+                  <AddCommentOutlinedIcon />
+                </IconButton>
+                <IconButton title="Share">
+                  <ShareOutlinedIcon />
+                </IconButton>
+                <IconButton title="Repost">
+                  <RepeatIcon />
+                </IconButton>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item md={12} xs={12}>
-            <Box
-              component="div"
-              sx={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignContent: "center",
-              }}
-            >
-              <IconButton title="Like">
-                <ThumbUpOutlinedIcon />
-              </IconButton>
-              <IconButton title="Add Comment">
-                <AddCommentOutlinedIcon />
-              </IconButton>
-              <IconButton title="Share">
-                <ShareOutlinedIcon />
-              </IconButton>
-              <IconButton title="Repost">
-                <RepeatIcon />
-              </IconButton>
-            </Box>
-          </Grid>
-        </Grid>
+        </Card>
       </Paper>
     </Box>
   );
