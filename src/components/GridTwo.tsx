b1a89662a0f10abd8b8499f14e7fd7e16c560177
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2024-01-15 04:36:58 +0100

    modified the select image and video func

--- src/components/GridTwo.tsx
+++ src/components/GridTwo.tsx
@@ -13,11 +13,11 @@ import PostAddIcon from "@mui/icons-material/PostAdd";
 import { useGeneral } from "../custom/hooks/useGeneral";
 import { getLoggedInUser } from "../Utilities/GetUserData";
 import { usePost } from "../custom/hooks/usePost";
+import { updateOtherState } from "../redux/Others/slice";
 const postStyle = {
   display: "flex",
   justifyContent: "space-around",
   flexDirection: "row",
-
 };
 
 export const GridTwo = () => {
@@ -29,19 +29,93 @@ export const GridTwo = () => {
     profileImageUrl,
     firstname,
     lastname,
+    others,
   } = useGeneral();
-  const {
-    content,
-    handleChangeContent,
-    selectedImage,
-    selectedVideo,
-  } = usePost();
+  const { content, handleChangeContent, setSelectedImage, setSelectedVideo } =
+    usePost();
   const userPhoto = async () => {
     try {
       await getLoggedInUser({ user, dispatch, aUser });
     } catch (err: any) {
       console.error("Error while fetching photo: ", err.message);
     }
+  };
+
+  const addVideo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let videoObj = e.target!.files![0];
+    const { size, type } = videoObj;
+    if (e.target.files?.length === 0) {
+      setSelectedVideo(undefined);
+      return false;
+    }
+    const mb = size / 1000000;
+    if (mb > 10) {
+      dispatch(
+        updateOtherState({
+          ...others,
+          open: true,
+          message: "Size exceeds the maximum allowed",
+          severity: "error",
+        })
+      );
+      e.target.value = "";
+      setSelectedVideo(undefined);
+      return false;
+    }
+    const ext = type.split("/")[0];
+
+    if (ext !== "video") {
+      dispatch(
+        updateOtherState({
+          ...others,
+          open: true,
+          message: "Please upload a video only",
+          severity: "error",
+        })
+      );
+      e.target.value = "";
+      setSelectedVideo(undefined);
+      return false;
+    }
+    setSelectedVideo(e.target!.files![0]);
+  };
+  const addImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.length === 0) {
+      setSelectedImage(undefined);
+      return false;
+    }
+    let imageObj = e.target!.files![0];
+    const { size, type } = imageObj;
+    const mb = size / 1000000;
+    if (mb > 2) {
+      dispatch(
+        updateOtherState({
+          ...others,
+          open: true,
+          message: "Size exceeds the maximum allowed",
+          severity: "error",
+        })
+      );
+      e.target.value = "";
+      setSelectedImage(undefined);
+      return false;
+    }
+    const ext = type.split("/")[0];
+
+    if (ext !== "image") {
+      dispatch(
+        updateOtherState({
+          ...others,
+          open: true,
+          message: "Please upload image only",
+          severity: "error",
+        })
+      );
+      e.target.value = "";
+      setSelectedImage(undefined);
+      return false;
+    }
+    setSelectedImage(e.target!.files![0]);
   };
 
   useEffect(() => {
@@ -81,7 +155,7 @@ export const GridTwo = () => {
                 value={content}
                 onChange={handleChangeContent}
                 size="small"
-                placeholder="Ask community for advice..."
+                placeholder="Talk with the community..."
               ></TextField>
             </Grid>
           </Grid>
@@ -91,6 +165,7 @@ export const GridTwo = () => {
               disableElevation
               variant="contained"
               size="small"
+              disabled={content.length <= 3}
             >
               Post
             </Button>
@@ -102,8 +177,7 @@ export const GridTwo = () => {
                 id="image"
                 type="file"
                 sx={{ display: "none" }}
-                name="selectedImage"
-                value={selectedImage}
+                onChange={addImage}
                 inputProps={{ accept: "image/*" }}
                 variant="standard"
                 InputProps={{
@@ -116,8 +190,7 @@ export const GridTwo = () => {
               Video
               <TextField
                 sx={{ display: "none" }}
-                name="selectedVideo"
-                value={selectedVideo}
+                onChange={addVideo}
                 type="file"
                 inputProps={{ accept: "video/*" }}
                 variant="standard"
