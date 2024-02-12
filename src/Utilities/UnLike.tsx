6dc4df21b3494d5669158d3e6bb75097682080a4
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2024-02-12 14:44:07 +0100

    added the post and article function together

--- src/Utilities/UnLike.tsx
+++ src/Utilities/UnLike.tsx
@@ -9,3 +9,12 @@ export const UnlikeArticle = async (articleId: any, likeId: string) => {
     console.error("Error deleting liked article", err.message);
   }
 };
+
+export const UnLikePost = async (postId: any, likeId: string) => {
+  try {
+    await deleteDoc(doc(db, "likePosts", postId, "likesPosts", likeId));
+    return "done";
+  } catch (err: any) {
+    console.error("Error deleting liked post", err.message);
+  }
+};
