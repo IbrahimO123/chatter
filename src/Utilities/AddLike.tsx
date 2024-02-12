50bce125d740145df52f907f4191474257bbec92
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2024-02-12 14:44:26 +0100

    added the post and article function together

--- src/Utilities/AddLike.tsx
+++ src/Utilities/AddLike.tsx
@@ -1,6 +1,6 @@
 import { collection, setDoc, doc } from "firebase/firestore";
 import { db } from "../config/firebase";
-import { LikeModel } from "../redux/like/model";
+import { LikeModel, LikePostModel } from "../redux/like/model";
 
 export const addLikeArticleToDatabase = async (
   data: LikeModel,
@@ -8,6 +8,20 @@ export const addLikeArticleToDatabase = async (
 ) => {
   try {
     const collectionRef = collection(db, "likes", articleId, "likes");
+    const res = await setDoc(doc(collectionRef), data);
+    return res;
+  } catch (e: any) {
+    console.error(e.message);
+  }
+};
+
+
+export const addLikePostToDatabase = async (
+  data: LikePostModel,
+  postId: string
+) => {
+  try {
+    const collectionRef = collection(db, "likePosts", postId, "likesPosts");
     const res = await setDoc(doc(collectionRef), data);
     return res;
   } catch (e: any) {
