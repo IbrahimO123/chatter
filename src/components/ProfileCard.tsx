269f946bf1e9e733413f344da0af4b7e2fdb5dbc
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2024-09-30 15:16:18 +0100

    adjust the profile card

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
 
 type ProfileCardType = {
   profileImg: string;
@@ -9,16 +9,29 @@ type ProfileCardType = {
 export const ProfileCard = ({ profileImg, author, email }: ProfileCardType) => {
   return (
     <section>
-      <Box  m={4} >
+      <Box m={4} mt={2} sx={{ display: "grid", placeItems: "center" }}>
         <Box component="div" sx={{ textAlign: "center" }}>
           <Avatar
             src={profileImg}
             title={author}
             alt={`${author} picture`}
+            sx={{ width: "100px", height: "100px" }}
           ></Avatar>
         </Box>
-        <Typography>Name: {author}</Typography>
-        <Typography>Email: {email} </Typography>
+        <Typography sx={{ margin: "2px" }}>Name: {author}</Typography>
+        {email ? <Typography>Email: {email} </Typography> : null}
+
+        <Button
+          sx={{
+            margin: "5px",
+            textTransform: "capitalize",
+            backgroundColor: "whitesmoke",
+            color: "black",
+            borderRadius: "5px",
+          }}
+        >
+          Follow
+        </Button>
       </Box>
     </section>
   );
